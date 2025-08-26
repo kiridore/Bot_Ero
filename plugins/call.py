e6cf6ae7fece04639eb4c334af877ffcf0497c21
kiridore <ashenone.jiang@gmail.com>
2025-08-26 15:35:30 +0800

    fix: 修复call插件被不断错误触发的问题

--- plugins/call.py
+++ plugins/call.py
@@ -3,7 +3,7 @@ from core.cq import text
 
 class CallPlugin(Plugin):
     def match(self):
-        return self.on_full_match("小埃同学") or self.only_to_me()
+        return self.on_full_match("小埃同学")
 
     def handle(self):
         self.send_msg(text("我在~"))
