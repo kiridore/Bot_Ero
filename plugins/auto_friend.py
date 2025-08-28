c61ad9634bfeb52433d71676be89e546a00dbe97
kiridore <ashenone.jiang@gmail.com>
2025-08-28 10:50:09 +0800

    fix: 尝试修复欢迎信息输出不正常的问题

--- plugins/auto_friend.py
+++ plugins/auto_friend.py
@@ -1,3 +1,4 @@
+from time import time
 from core.base import Plugin
 from core.cq import text
 
@@ -5,8 +6,10 @@ class AutoFriendPlugin(Plugin):
     def match(self):
         if self.context.get("request_type", "") == "friend":
             return True
+        return False
 
     def handle(self):
         flag = self.context["flag"]
         self.set_friend_add_request(flag)
+        time.sleep(1)
         self.send_private_msg(text("感谢订阅小埃同学私人打卡服务喵~\n 使用指令“菜单”即可查看所有可用功能"))
