ab12a40081754decbceb35c9754e38aacbdfa738
kiridore <ashenone.jiang@gmail.com>
2026-01-05 01:05:35 +0800

    fix(checkin): 修复打卡多张图片时无法正确判断是否为当周首次打卡的问题

--- plugins/checkin.py
+++ plugins/checkin.py
@@ -20,13 +20,20 @@ class CheckinPlugin(Plugin):
             for img_name in img_list :
                 # 找到的图片列表
                 logger.debug("{}".format(self.get_image(img_name)))
+
             start_date, end_date = get_monday_to_monday()
+
+            # 确定是否首次打卡
+            before_checkin_list = self.dbmanager.search_target_user_checkin_range(self.context["user_id"], start_date, end_date)
+            is_first = False
+            if len(before_checkin_list) == 0:
+                is_first = True
 
             #先打卡后搜索
             self.dbmanager.insert_checkin(self.context["user_id"], img_list)
             checkin_list = self.dbmanager.search_target_user_checkin_range(self.context["user_id"], start_date, end_date)
-            if len(checkin_list) == 1:
+            if is_first:
                 add_user_point(self.dbmanager, self.context['user_id'], 1)
-                self.send_msg(at(self.context["user_id"]), text("\n🌟打卡成功喵🌟\n收录了{}张图片\n完成本周首次打卡喵，拿好你的积分~".format(len(img_list), len(checkin_list))))
+                self.send_msg(at(self.context["user_id"]), text("\n🌟打卡成功喵🌟\n收录了{}张图片\n完成本周首次打卡喵，拿好你的积分~".format(len(img_list))))
             else:
-                self.send_msg(at(self.context["user_id"]), text("\n⭐打卡成功喵⭐\n收录了{}张图片\n完成本周第{}次打卡喵".format(len(img_list), len(checkin_list))))
+                self.send_msg(at(self.context["user_id"]), text("\n⭐打卡成功喵⭐\n收录了{}张图片\n这周已经提交了{}张图了喵".format(len(img_list), len(checkin_list))))
