5628478d6857a78c842a50acbefc952fff8625d3
kiridore <ashenone.jiang@gmail.com>
2026-01-05 01:05:55 +0800

    fix(roll_back): 修复撤回当周唯一一次打卡时不会扣除积分的问题

--- plugins/roll_back.py
+++ plugins/roll_back.py
@@ -1,3 +1,4 @@
+from core import utils
 from core.base import Plugin
 from core.cq import text,image
 from core.utils import get_monday_to_monday
@@ -17,4 +18,9 @@ class RollbackCheckinPlugin(Plugin):
             del_time = rows[0][2]
             logger.debug(rows)
             self.send_msg(text("成功撤回了本周最近一次打卡喵:\n{}".format(del_time)), image(del_image))
+
+            if len(rows) == 1:
+                self.send_msg(text("你刚刚撤回了本周第一次打卡，需要扣掉本周的点数喵！"))
+                utils.add_user_point(self.dbmanager, self.context['user_id'], -1)
+
             self.dbmanager.delete_checkin_by_id(rows[0][0])
