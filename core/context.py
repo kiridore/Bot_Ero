54cd3047c4b69113347b70d2f49d6d2c596addaf
kiridore <ashenone.jiang@gmail.com>
2026-04-10 04:22:25 +0800

    feat: 开启开机动画

--- core/context.py
+++ core/context.py
@@ -6,5 +6,5 @@ should_shutdown = False
 llonebot_data_path = "/app/llonebot/server_data"    # 使用api是用这个地址
 python_data_path = "./server_data"                  # 在python脚本中访问用这个地址
 onebot_qq_volume = "/var/lib/docker/volumes/onebot_qq_volume/_data"
-startup_changelog_sent = True
+startup_changelog_sent = False
 recent_chat_records = []
