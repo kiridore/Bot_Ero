20de07d579fa51eb047cccca3d685cfcce6aa0e1
kiridore <ashenone.jiang@gmail.com>
2026-04-17 03:16:53 +0800

    feat: 删除废弃的全局变量

--- core/context.py
+++ core/context.py
@@ -6,7 +6,6 @@ if TYPE_CHECKING:
     from core.base import Plugin
 
 script_start_time = datetime.now()
-should_shutdown = False
 llonebot_data_path = "/app/llonebot/server_data"    # 使用api是用这个地址
 python_data_path = "./server_data"                  # 在python脚本中访问用这个地址
 onebot_qq_volume = "/var/lib/docker/volumes/onebot_qq_volume/_data"
--- plugins/update.py
+++ plugins/update.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from core import context
+from core import api, context
 from core.base import Plugin
 from core.cq import text
 import git
@@ -30,7 +30,6 @@ class UpdatePlugin(Plugin):
             commits = list(repo.iter_commits(commit_range))
             self.api.send_msg(text(f"更新了 {len(commits)} 个 补丁，准备重启了喵~"))
             # 更新成功并重启
-            context.should_shutdown = True
             os.execv(sys.executable, ['python3'] + sys.argv)
         else:
             self.api.send_msg(text("小埃同学已经是最新版本了喵~"))
